dd6eed37bff7fb684aab222b41de45aef2b6b626
PlainOldKevin <61211742+PlainOldKevin@users.noreply.github.com>
2022-03-28 16:12:32 -0400

    did a little work

--- app/src/main/java/com/example/livesportsodds/MainActivity.java
+++ app/src/main/java/com/example/livesportsodds/MainActivity.java
@@ -8,6 +8,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String[] sportsGroups = new String[0];
+    boolean userSelect = false;
+    private String url1 = "https://odds.p.rapidapi.com/v1/sports";
+    private String url2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
