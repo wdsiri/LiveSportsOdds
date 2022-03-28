488b6710c548bf28cf93be550bdfc761142c4a26
wsiri <wsiri@ips-yes.com>
2022-03-28 14:14:58 -0400

    Initial commit

---
+++ app/src/main/java/com/example/livesportsodds/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.livesportsodds;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.GridLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
