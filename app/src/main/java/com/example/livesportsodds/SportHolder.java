cbabcf480fcb841c26f49d573d3ac26832aaf6a4
PlainOldKevin <61211742+PlainOldKevin@users.noreply.github.com>
2022-04-12 15:02:08 -0400

    SportHolder array issue

---
+++ app/src/main/java/com/example/livesportsodds/SportHolder.java
@@ -0,0 +1,42 @@
+package com.example.livesportsodds;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SportHolder {
+
+    public static final String [] sports = new String[0];
+
+    public SportHolder() {
+
+
+
+    }
+
+    public String getSport(String sportJsonStr) throws JSONException {
+        JSONObject sportJSONObj = new JSONObject(sportJsonStr);
+        System.out.println(sportJSONObj);
+        JSONArray dataArray = sportJSONObj.getJSONArray("data");
+        // write a loop where the array elements and put the array elements in your own array of sports
+        // dataArray.get(i).getString("sport");
+
+        if (dataArray != null) {
+          //  System.out.println(dataArray.length());
+            for (int i = 0; i < dataArray.length(); i++) {
+             //   System.out.println(i);
+                JSONObject obj = dataArray.getJSONObject(i);
+            //    System.out.println(i);
+                if (obj != null) {
+                    System.out.println(obj.toString());
+                    sports[i] = obj.getString("group");
+                    System.out.println(sports[i]);
+                }
+                else {
+                    System.out.println("object is null");
+                }
+            }
+        }
+        return null;
+    }
+}
